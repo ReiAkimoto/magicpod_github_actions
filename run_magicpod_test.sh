a89361c33b4f151280c12b8e78130477bba779ba
ReiAkimoto <104745583+ReiAkimoto@users.noreply.github.com>
2023-09-20 17:22:04 +0900

    Update run_magicpod_test.sh

--- run_magicpod_test.sh
+++ run_magicpod_test.sh
@@ -3,7 +3,9 @@
 
 # magicpod-apiクライアントの最新版を現在のディレクトリにダウンロード・解凍
 # セキュリティのため、MAGICPOD_API_TOKENはGitHubの環境変数で設定
-OS=mac 　# Windowsマシン上でのビルドの場合はwindows、Linuxはlinuxを指定
+# Windowsマシン上でのビルドの場合はwindows、Linuxはlinuxを指定
+OS=mac
+# 任意のファイル名
 FILENAME=magicpod-api-client
 curl -L "https://app.magicpod.com/api/v1.0/magicpod-clients/api/${OS}/latest/" -H "Authorization: Token ${MAGICPOD_API_TOKEN}" --output ${FILENAME}.zip
 unzip -q ${FILENAME}.zip
